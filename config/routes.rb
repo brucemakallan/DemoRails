6c47343de9ea642ff795f6f9275e212ea4adcb8d
Bruce Makaaru <brucemakallan@gmail.com>
2018-04-15 13:11:36 +0300

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :servers
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'servers#index'
+end
